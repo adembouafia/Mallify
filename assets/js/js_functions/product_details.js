166530406727d09c1700768d9b99231be8b32337
abroud koussay <abkoussay40@gmail.com>
2025-04-11 15:40:35 +0100

    edited

--- assets/js/js_functions/product_details.js
+++ assets/js/js_functions/product_details.js
@@ -4,26 +4,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     plusButtons.forEach(button => {
         button.addEventListener("click", function () {
-            const input = this.closest("td").querySelector(".quantity__input");
+            const input = this.closest(".d-flex").querySelector(".quantity__input");
             let value = parseInt(input.value, 10);
             value++;
             input.value = value;
         });
     });
+
     minusButtons.forEach(button => {
         button.addEventListener("click", function () {
-            const input = this.closest("td").querySelector(".quantity__input");
+            const input = this.closest(".d-flex").querySelector(".quantity__input");
             let value = parseInt(input.value, 10);
             if (value > 1) {
                 value--;
             }
             input.value = value;
-        });
-    });
-
-    document.querySelectorAll(".remove-tr-btn").forEach(button => {
-        button.addEventListener("click", function () {
-            this.closest("tr").classList.add("d-none");
         });
     });
 });
