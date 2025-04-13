c61a2095b219369b4dc6a5b82f50e908821b6dd6
adem bouafia <adembouafia0@gmail.com>
2025-04-13 18:52:43 +0100

    adding dashbord shop

---
+++ assets/js/js_functions/shopAdmin.js
@@ -0,0 +1,80 @@
+//sidebar toggle
+document.addEventListener('DOMContentLoaded', () => {
+    // Constants
+    const CLASS_NAME_SIDEBAR_COLLAPSE = 'sidebar-collapse';
+    const CLASS_NAME_SIDEBAR_OPEN = 'sidebar-open';
+    const SELECTOR_SIDEBAR_TOGGLE = '[data-lte-toggle="sidebar"]';
+    const SELECTOR_APP_SIDEBAR = '.app-sidebar';
+    // PushMenu Class
+    class PushMenu {
+        constructor(element) {
+            this._element = element;
+            this._sidebar = document.querySelector(SELECTOR_APP_SIDEBAR);
+        }
+
+        // Toggle the sidebar
+        toggle() {
+            if (document.body.classList.contains(CLASS_NAME_SIDEBAR_COLLAPSE)) {
+                this.expand();
+            } else {
+                this.collapse();
+            }
+        }
+
+        // Expand the sidebar
+        expand() {
+            document.body.classList.remove(CLASS_NAME_SIDEBAR_COLLAPSE);
+            document.body.classList.add(CLASS_NAME_SIDEBAR_OPEN);
+        }
+
+        // Collapse the sidebar
+        collapse() {
+            document.body.classList.remove(CLASS_NAME_SIDEBAR_OPEN);
+            document.body.classList.add(CLASS_NAME_SIDEBAR_COLLAPSE);
+        }
+
+    }
+
+    // Initialize PushMenu and add event listeners
+    const sidebarToggle = document.querySelectorAll(SELECTOR_SIDEBAR_TOGGLE);
+    sidebarToggle.forEach(btn => {
+        const pushMenu = new PushMenu(btn);
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            pushMenu.toggle();
+        });
+    });
+});
+
+
+
+//report cards toggle
+document.addEventListener('DOMContentLoaded', () => {
+    const SELECTOR_COLLAPSE = '[data-lte-toggle="card-collapse"]';
+    const SELECTOR_REMOVE = '[data-lte-toggle="card-remove"]';
+    const SELECTOR_MAXIMIZE = '[data-lte-toggle="card-maximize"]';
+    document.querySelectorAll(SELECTOR_COLLAPSE).forEach(button => {
+        button.addEventListener('click', () => {
+            const card = button.closest('.card');
+            card.classList.toggle('collapsed-card');
+        });
+    });
+
+    document.querySelectorAll(SELECTOR_REMOVE).forEach(button => {
+        button.addEventListener('click', () => {
+            const card = button.closest('.card');
+            card.remove();
+        });
+    });
+
+    document.querySelectorAll(SELECTOR_MAXIMIZE).forEach(button => {
+        button.addEventListener('click', () => {
+            const card = button.closest('.card');
+            card.classList.toggle('maximized-card');
+    
+            const html = document.documentElement;
+            html.classList.toggle('maximized-card');
+        });
+    });
+});
+
