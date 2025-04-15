a765b0d454ca0835cb9b1dde9beaa8051c0c1e53
abroud koussay <abkoussay40@gmail.com>
2025-04-15 19:05:03 +0100

    edited and fixed issues

--- assets/js/js_functions/mainProfil.js
+++ assets/js/js_functions/mainProfil.js
@@ -21,3 +21,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     showContent('profile-info');
 });
+
+/*hide and show password  */
+document.querySelectorAll('.toggle-password').forEach(icon => {
+    icon.addEventListener('click', function() {
+        const targetId = this.getAttribute('data-target');
+        const input = document.getElementById(targetId);
+        const isPassword = input.type === 'password';
+        input.type = isPassword ? 'text' : 'password';
+        this.classList.toggle('ph-eye-slash');
+        this.classList.toggle('ph-eye');
+    });
+  });
