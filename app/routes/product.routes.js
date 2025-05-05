06ed35e0a228b6f2b003b7c04a6dc9c58e4772fd
adem bouafia <adembouafia0@gmail.com>
2025-05-05 12:24:02 +0100

    fixess

--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
     const product = require("../controllers/product.controller");
     app.post("/product/create",auth ,product.createProduct);
     app.get("/product/get", product.getAllProducts);
-    app.get("/product/get/myProducts", product.getMyProducts);
+    app.get("/product/get/myProducts", auth , authorize('vendor' , 'moderator'),product.getMyProducts);
     app.get("/product/get/:id", product.getProduct);
     app.put("/product/update/:id", auth, authorize('vendor', 'moderator'), product.updateProduct);
     app.delete("/product/delete/:id", auth, authorize('vendor', 'moderator'), product.deleteProduct);
