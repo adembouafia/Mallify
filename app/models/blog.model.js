1bfdad94652dab2223eabf41a64d9112f16b1083
adem bouafia <adembouafia0@gmail.com>
2025-05-07 20:45:24 +0100

    add blog-dash but some issues there

--- app/models/blog.model.js
+++ app/models/blog.model.js
@@ -1,5 +1,28 @@
 const mongoose = require("mongoose");
 
+const commentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
 const blogSchema = new mongoose.Schema({
     mainImageBlog: {
         type: String,
@@ -7,7 +30,7 @@ const blogSchema = new mongoose.Schema({
     },
     otherImagesBlog: {
         type: [String],
-        required: true,
+        default: []
     },
     title: {
         type: String,
@@ -20,10 +43,31 @@ const blogSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true,
+    },
+    category: {
+        type: String,
+        default: "Non catégorisé"
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    status: {
+        type: String,
+        enum: ["published", "draft"],
+        default: "published"
+    },
+    comments: [commentSchema],
+    views: {
+        type: Number,
+        default: 0
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     }
 },{
-    timestamps: true,
-})
-
+    timestamps: true
+});
 
 module.exports = mongoose.model("Blog", blogSchema);
